1d1c2335f1edbe317f9689f5bf17a9010cd6076e
Adam <adam.kjall@hotmail.com>
2019-11-07 17:32:05 +0100

    made the hero background image  fixed,  clicking on menu item now closes the menu and added a background color to the navbar when scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,15 @@ const hamburgerElement = document.querySelector("nav .hamburger");
 const menuElement = document.querySelector("nav .menu");
 const projectsElement = document.querySelector(".menu .projects");
 const accordionElement = document.querySelector(".menu .accordion");
+const linkElements = document.querySelectorAll(".menu li a");
+const navElement = document.querySelector("nav");
 
 hamburgerElement.addEventListener("click", toggleMenu);
 projectsElement.addEventListener("click", toggleAccordion);
+
+linkElements.forEach(el => {
+  el.addEventListener("click", toggleMenu)
+})
 
 
 function toggleMenu() {
@@ -15,5 +21,19 @@ function toggleMenu() {
 function toggleAccordion() {
   projectsElement.classList.toggle("open")
   accordionElement.classList.toggle("show");
-
 }
+
+function showNavOnScroll() {
+  window.onscroll = () => {
+    if (window.pageYOffset > window.innerHeight) {
+      navElement.classList.add("nav-background")
+    }
+    else {
+      navElement.classList.remove("nav-background");
+    }
+  }
+}
+
+showNavOnScroll();
+
+
