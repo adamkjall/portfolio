c5b5108b9ad8acfede2c040721cb3a3409bbcc8b
Adam <adam.kjall@hotmail.com>
2019-11-13 10:34:21 +0100

    changed the accordion transition effect

--- scripts/script.js
+++ scripts/script.js
@@ -11,43 +11,49 @@ const bandElements = document.querySelectorAll(".band");
 function initListeners() {
   hamburgerElement.addEventListener("click", toggleMenu);
   projectsElement.addEventListener("click", toggleAccordion);
-  
+
   linkElements.forEach(el => {
-    el.addEventListener("click", toggleMenu)
-  })
-  
-  
+    el.addEventListener("click", toggleMenu);
+  });
+
   bandElements.forEach(el =>
     el.addEventListener("click", e => {
       el.classList.toggle("hide-animation");
     })
   );
-}
 
+  window.addEventListener("click", e => {
+    if (navElement.contains(e.target)) {
+      return;
+    }
+    closeMenu();
+  })
+}
 
 function toggleMenu() {
   menuElement.classList.toggle("menu-hide");
   hamburgerElement.classList.toggle("is-active");
 }
 
+function closeMenu() {
+  menuElement.classList.add("menu-hide");
+  hamburgerElement.classList.remove("is-active");
+}
+
 function toggleAccordion() {
-  projectsElement.classList.toggle("open")
+  projectsElement.classList.toggle("open");
   accordionElement.classList.toggle("show");
 }
 
 function showNavOnScroll() {
   window.onscroll = () => {
     if (window.pageYOffset > window.innerHeight - 5) {
-      navElement.classList.add("nav-background")
-    }
-    else {
+      navElement.classList.add("nav-background");
+    } else {
       navElement.classList.remove("nav-background");
     }
-  }
+  };
 }
-
 
 initListeners();
 showNavOnScroll();
-
-
